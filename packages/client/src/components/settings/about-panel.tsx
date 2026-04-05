fc75d5f4ce55f4ccf14ce22f0582c20d14d5ff18
gorkem-bwl <gorkem.cetin@bluewavelabs.ca>
2026-04-05 14:06:02 -0400

    chore: bump version to 1.5.2

--- packages/client/src/components/settings/about-panel.tsx
+++ packages/client/src/components/settings/about-panel.tsx
@@ -51,7 +51,7 @@ export function AboutPanel() {
               border: '1px solid var(--color-border-secondary)',
             }}
           >
-            1.5.1
+            1.5.2
           </span>
         </SettingsRow>
 
